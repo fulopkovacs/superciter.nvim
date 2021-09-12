e30e1722b22ba3b6d1b1fbe044b2716c82cd5401
Fülöp Kovács <kovacs.fulop@gmail.com>
2021-09-12 15:50:50 +0200

    [fixup later] Make a table that we can use to display the matches

--- query4.lua
+++ query4.lua
@@ -16,8 +16,18 @@ local start_range, _, end_range, _ = document:range()
 -- local query = [[
 -- ((identifier)@i (value)@v (#eq? @i "title"))
 -- ]]
+-- local query = [[
+-- ((identifier)@i (value)@v (#any-of? @i "title" "author"))
+-- ]]
 local query = [[
-((identifier)@i (value)@v (#any-of? @i "title" "author"))
+(
+  (key_brace)@k
+  (field (
+    (identifier) @i
+    (value) @v
+    (#any-of? @i "title" "author" "year")
+  ))
+)
 ]]
 
 -- SOME VALID QUERIES
@@ -57,7 +67,7 @@ local parsed_query = ts.parse_query('bibtex', query)
 
 local ids = {}
 local titles = {}
-local authors = {}
+local bib_objects = {}
 
 --[[
 1. set id in metadata
@@ -67,7 +77,7 @@ local entries = {ab94= {author, title, year}}
 
 -- Remove enclosing "-s and {}-s
 local function strip_value(value)
-    if value:sub(1, 1) == '"' or value:sub(1, 1) == '{' then value = string.sub(value, 2, #value - 2) end
+    if value:sub(1, 1) == '"' or value:sub(1, 1) == '{' then value = string.sub(value, 2, #value - 1) end
     return value
 end
 
@@ -78,30 +88,51 @@ local function get_text_value(bufnr, node)
     local text = ''
     if row1 == row2 then
         -- Every word in the title is on the same line
-        text = string.sub(lines[1], col1 + 1, col2 + 1)
+        text = string.sub(lines[1], col1 + 1, col2)
     else
         -- The title is broken into multiple lines
         lines[1] = string.sub(lines[1], col1 + 1, -1)
-        lines[#lines] = string.sub(lines[#lines], 0, col2 + 1)
+        lines[#lines] = string.sub(lines[#lines], 0, col2)
         text = table.concat(lines, ' ')
     end
     return strip_value(text)
 end
 
 -- Collect the nodes
-for id, node, metadata in parsed_query:iter_captures(document, bufnr, start_range, end_range) do
+local match_id = 0
+local captured_key_braces = {}
+for pattern, match, metadata in parsed_query:iter_matches(document, bufnr, start_range, end_range) do
+  match_id = match_id + 1
+  local matched_value = ''
+  local matched_identifier = ''
+  for id, node in pairs(match) do
     local captured_name = parsed_query.captures[id] -- name of the capture in the query
     local type = node:type()
     local text = get_text_value(bufnr, node)
-    print(id, type, text)
-    if type == "value" then table.insert(titles, text) end
-    -- if id ~= 1 then break end -- TODO: remove after testing
+    if type == "value" then
+      matched_value = text
+    end
+    if type == "identifier" then
+      matched_identifier = text
+    end
+    if type == "key_brace" then
+      table.insert(captured_key_braces, text)
+    end
+    -- if type ~= "key_brace" then print(match_id, captured_key_braces[match_id], id, type, text) end
+  end
+  -- print(captured_key_braces[match_id])
+  local matched_key_brace = captured_key_braces[match_id]
+  if bib_objects[matched_key_brace] == nil then bib_objects[matched_key_brace] = {} end
+  -- bib_objects[matched_key_brace] = {}
+  bib_objects[matched_key_brace][matched_identifier] = matched_value
 end
 
 -- if #ids == #titles then for i = 1, #titles do print(ids[i], titles[i]) end end
 -- print(#ids)
-print(#titles)
+-- print(#titles)
 -- print(titles[3])
 -- print(vim.inspect(titles))
 -- for i = 1, #titles do print(titles[i]) end
+-- for i = 1, #captured_key_braces do print(captured_key_braces[i]) end
 -- print(titles[1])
+print(vim.inspect(bib_objects))
