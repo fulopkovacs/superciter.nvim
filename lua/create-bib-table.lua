91704e876370c45e7261220367a5966570161cd6
Fülöp Kovács <kovacs.fulop@gmail.com>
2021-11-22 23:37:53 +0100

    Sort the displayed bib entries by the name of the first author

--- lua/create-bib-table.lua
+++ lua/create-bib-table.lua
@@ -3,9 +3,7 @@ Return a table with the relevant information for every entry in the bib file..
 ]] --
 -- Remove enclosing "-s and {}-s
 local function strip_value(value)
-    if value:sub(1, 1) == '"' or value:sub(1, 1) == '{' then
-        value = string.sub(value, 2, #value - 1)
-    end
+    if value:sub(1, 1) == '"' or value:sub(1, 1) == '{' then value = string.sub(value, 2, #value - 1) end
     return value
 end
 
@@ -38,15 +36,28 @@ local function update_bib_table(bib_table)
     return updated_bib_table
 end
 
+-- Sort the table of the bib entries by the name of the first authors
+local function sort_by_author(entry_1, entry_2)
+    -- Make sure that the entries without titles will be ranked lower than
+    -- the entries with titles
+    if entry_1["author"] == nil and entry_2 then return false end
+    if entry_2["author"] == nil and entry_1 then return true end
+    if entry_1 == nil and entry_2 == nil then return true end
+
+    if entry_1['author'] < entry_2['author'] then
+        return true
+    else
+        return false
+    end
+end
+
 -- Get all the relevant info from the bib file nodes
 local function get_bib_info(parsed_query, document, bufnr, start_range, end_range)
     local bib_table = {}
     -- Collect the nodes
     local match_id = 0
     local captured_key_braces = {}
-    for _, match, _ in parsed_query:iter_matches(document, bufnr,
-                                                              start_range,
-                                                              end_range) do
+    for _, match, _ in parsed_query:iter_matches(document, bufnr, start_range, end_range) do
         match_id = match_id + 1
         local matched_value = ''
         local matched_identifier = ''
@@ -55,14 +66,10 @@ local function get_bib_info(parsed_query, document, bufnr, start_range, end_rang
             local text = get_text_value(bufnr, node)
             if type == "value" then matched_value = text end
             if type == "identifier" then matched_identifier = text end
-            if type == "key_brace" then
-                table.insert(captured_key_braces, text)
-            end
+            if type == "key_brace" then table.insert(captured_key_braces, text) end
         end
         local matched_key_brace = captured_key_braces[match_id]
-        if bib_table[matched_key_brace] == nil then
-            bib_table[matched_key_brace] = {}
-        end
+        if bib_table[matched_key_brace] == nil then bib_table[matched_key_brace] = {} end
         bib_table[matched_key_brace][matched_identifier] = matched_value
     end
     return bib_table
@@ -96,13 +103,13 @@ local function create_bib_table(bufnr)
 
     local parsed_query = ts.parse_query('bibtex', query)
 
-    local bib_table = get_bib_info(parsed_query, document, bufnr, start_range,  end_range)
+    local bib_table = get_bib_info(parsed_query, document, bufnr, start_range, end_range)
 
     bib_table = update_bib_table(bib_table)
+
+    table.sort(bib_table, sort_by_author)
 
     return bib_table
 end
 
-return {
-  get_bib_info = create_bib_table
-}
+return {get_bib_info = create_bib_table}
